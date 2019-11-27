280dfda6380a0ea5bfd69df36035891716f3d1ec
arzwa <arzwa@psb.vib.ugent.be>
2019-11-27 16:45:43 +0100

    removed deps

--- test/_rjmcmc.jl
+++ test/_rjmcmc.jl
@@ -1,5 +1,5 @@
 using Distributed
-# addprocs(2)
+addprocs(2)
 @everywhere using Pkg
 @everywhere Pkg.activate("/home/arzwa/dev/Beluga/")
 @everywhere using Beluga, PhyloTree
@@ -16,7 +16,7 @@ begin
     df = CSV.read("test/data/plants1-100.tsv", delim=",")
     # df = CSV.read("test/data/N=250_tree=plants1c.nw_η=0.9_λ=2_μ=2.csv", delim=",")
     # df = CSV.read("../../rjumpwgd/data/sims/model1_8wgd_N=1000.csv", delim=",")
-    df = df[1:20,:]
+    df = df[1:100,:]
     nw = open("test/data/plants1c.nw", "r") do f ; readline(f); end
     d, y = DuplicationLossWGDModel(nw, df, exp(randn()), exp(randn()), 0.9, Beluga.BelugaBranch)
     p = Profile(y)
@@ -24,7 +24,7 @@ begin
     prior = IidRevJumpPrior(
         Σ₀=[1 0.9 ; 0.9 1],
         X₀=MvNormal(log.([2,2]), I),
-        πK=Beluga.UpperBoundedGeometric(0.2, 15),
+        πK=Beluga.UpperBoundedGeometric(0.3, 15),
         πq=Beta(1,1),
         πη=0.9)
     chain = RevJumpChain(data=p, model=deepcopy(d), prior=prior)
