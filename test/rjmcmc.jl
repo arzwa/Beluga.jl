45c86d062ed1eec964c50cb852c906fe1bd7883b
Arthur ZWAENEPOEL <arzwa@psb.ugent.be>
2020-01-14 14:10:45 +0100

    DA verified

--- test/rjmcmc.jl
+++ test/rjmcmc.jl
@@ -52,7 +52,7 @@ end
 
 begin
     nw = readline("example/9dicots/dicots.nw")
-    df = CSV.read("example/9dicots/dicots-f01-25.csv")
+    df = CSV.read("example/9dicots/dicots-f01-100.csv")
     d, p = DLWGD(nw, df, 2., 2., 0.9)
     # d, p = DLWGD(nw, 2., 2., 0.9)
     prior = IRRevJumpPrior(
@@ -61,7 +61,8 @@ begin
         πK=DiscreteUniform(0, 10),
         πq=Beta(1,3),
         πη=Beta(3,1),
-        Tl=Beluga.treelength(d))
+        Tl=Beluga.treelength(d),
+        πE=Normal(1,0.05))
     kernel = Beluga.BranchKernel(qkernel=Beta(1,3))
     props = Beluga.AMMProposals(2*Beluga.ne(d)+2, σ=0.1, β=0.05)
     chain = RevJumpChain(data=p, model=d, prior=prior,
@@ -70,6 +71,9 @@ begin
     for i=1:100
         @time rjmcmc!(chain, 100, trace=1, show=10)
         p = plot([plot(chain.trace[!,Symbol("λ$i")]) for i=1:4:16]...,
+                 [stephist(log10.(chain.trace[!,Symbol("λ$i")]),
+                    fill=true, normalize=true, alpha=0.5) for i=1:4:16]...,
+                  plot(chain.trace[!,:k]),
                  legend=false, grid=false)
         display(p)
     end
